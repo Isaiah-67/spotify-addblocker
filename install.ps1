9a22f0b4717c7e54351b2d82bec5d3030bd34c5f
Isaiah <84519049+Isaiah-67@users.noreply.github.com>
2022-07-25 11:09:21 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,5 @@
-<taskbar:DesktopApp DesktopApplicationLinkPath="C:\Program Files\iTunes\iTunes.exe" />
+$shell = new-object -com "Shell.Application"  
+$folder = $shell.Namespace('C:\Windows')    
+$item = $folder.Parsename('notepad.exe')
+$verb = $item.Verbs() | ? {$_.Name -eq 'Pin to Tas&kbar'}
+if ($verb) {$verb.DoIt()}
