452d87103720ce1fda87af81ab11d2c29deb9eed
Isaiah <84519049+Isaiah-67@users.noreply.github.com>
2022-07-25 11:09:38 -0400

    Delete install.ps1

--- install.ps1
+++
@@ -1,5 +0,0 @@
-$shell = new-object -com "Shell.Application"  
-$folder = $shell.Namespace('C:\Windows')    
-$item = $folder.Parsename('notepad.exe')
-$verb = $item.Verbs() | ? {$_.Name -eq 'Pin to Tas&kbar'}
-if ($verb) {$verb.DoIt()}
