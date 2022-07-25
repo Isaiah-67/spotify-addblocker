d71dd40a93af751fcedec68f16dbf22e775f5e68
Isaiah <84519049+Isaiah-67@users.noreply.github.com>
2022-07-25 11:20:29 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,1 @@
-$shell = new-object -com "Shell.Application"  
-$folder = $shell.Namespace('Desktop/iTunes.lnk')    
-$item = $folder.Parsename('iTunes.exe')
-$verb = $item.Verbs() | ? {$_.Name -eq 'Pin to Taskbar'}
-if ($verb) {$verb.DoIt()}
+Add-TaskbarPinning("C:\Windows\Notepad.exe")
